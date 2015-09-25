332e9a17fab91c93d54be8c1c83bdae6dd31a176
Rafael C. Nunes <rafaelnunes737@hotmail.com>
2015-09-24 22:18:11 -0300

    Função adicionada e refinada.

--- src/server/server-utils.js
+++ src/server/server-utils.js
@@ -7,5 +7,13 @@ var path = require('path');
 
 // Diretório absoluto para a pasta pública - do cliente -.
 exports.public_dir = function() {
-  return path.join(process.cwd(), "../public/");
+    return path.join(process.cwd(), "../public/");
+}
+
+/**
+ *
+ */
+exports.get_file = function(file) {
+    var public_dir = path.join(process.cwd(), "../public/");
+    return path.join(public_dir, file);
 }
