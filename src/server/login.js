daeb69928fd174649ce2b632d6042f7d3a7ea191
Rafael C. Nunes <rafaelnunes737@hotmail.com>
2015-10-03 15:58:28 -0300

    Script de login um pouco mais maturo

--- src/server/login.js
+++ src/server/login.js
@@ -7,18 +7,60 @@
 
 // node.js
 var path = require('path');
-//
-var ws = require('./web-socket.js')
+// WebSocket
+var ws = require('./web-socket.js');
 // SCE
-var utils = require('./server-utils.js')
+var utils = require('./server-utils.js');
+var mysql = require('./db_pool.js');
+var db_api  = require('./db_api.js');
 
+
+/**
+ *
+ * @param req
+ * @param res
+ */
 exports.do_login = function(req, res) {
-   var email = req.usuario.email;
-   var senha = req.usuario.senha;
+    var email = req.usuario.email;
+    var senha = req.usuario.senha;
 
-   var index_page = path.join(utils.public_dir(), "index.html");
+    db_api.getUsuarios(function callback(data, err) {
+        // se erro estiver definido.
+        if(err) {
+            console.log("[DB_API] Erro de requisição: " + err)
+            ws.send_json({
+              code: '1004',
+              desc: err
+            })
+            res.sendFile(utils.get_file("login.html"));
+        }
+        else {
+            for(var i = 0; i < data.length; i++) {
+                console.log(data[i].email + " - " + data[i].senha)
+                if(email == data[i].email && senha == data[i].senha) {
+                    ws.send_json({
+                      code: '1000',
+                      desc: 'Usuário autorizado.'
+                    });
+                    //TODO: Criar sessão do usuário (cookies).
+                    res.sendFile(utils.get_file("/index.html"));
+                }
+                else {
+                    if(i == (data.length - 1)) {
+                        ws.send_json({
+                          code: '1001',
+                          desc: 'Usuário não autorizado.'
+                        });
+                        res.sendFile(utils.get_file("login.html"));
+                        break;
+                    }
+                }
+            }
+        }
+    });
+ }
 
-   res.sendFile(index_page);
-   ws.send_message("login error");
-   //ws.send_json("error: asdsad");
+ exports.logout = function(req, res) {
+   console.log('logging out!');
+   res.sendFile(utils.get_file("login.html"));
  }
