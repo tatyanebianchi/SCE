71a4534bcd3ccacfcc834b3c62d1a3da09cd7e55
Rafael Campos Nunes <rafaelnunes737@hotmail.com>
2015-10-10 20:24:21 -0300

    Padronização das funções utilizadas no sistema.

--- src/server/db_api.js
+++ src/server/db_api.js
@@ -10,27 +10,40 @@ var mysql_pool = require('./db_pool.js');
 /**
  * @param {Function} callback
  */
-exports.getEmpresas = function(return_data) {
+exports.get_empresas = function(return_data) {
     mysql_pool.query("SELECT * FROM sce.empresa;", return_data);
 }
 
 /**
  * @param {Function} callback
  */
-exports.getOrientadores = function(return_data) {
+exports.get_orientadores = function(return_data) {
     mysql_pool.query("SELECT * FROM sce.orientador;", return_data);
 }
 
 /**
  * @param {Function} callback
  */
-exports.getEstagiarios = function(return_data) {
+exports.get_estagiarios = function(return_data) {
     mysql_pool.query("SELECT * FROM sce.estagiario;", return_data);
 }
 
 /**
  * @param {Function} callback
  */
-exports.getUsuarios = function(return_data) {
-  mysql_pool.query("SELECT * FROM sce.usuario;", return_data);
+exports.get_usuarios = function(return_data) {
+    mysql_pool.query("SELECT * FROM sce.usuario;", return_data);
+}
+
+
+exports.search_estagiario = function(search, return_data) {
+    mysql_pool.query("SELECT * FROM sce.estagiario WHERE sce.estagiario.nome LIKE " + search + "%");
+}
+
+exports.insert_estagiario = function(callback) {
+    //mysql_pool.query()
+}
+
+exports.insert_empresa = function(callback) {
+
 }
