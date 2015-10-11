a6cca37a68f8aacb900b5bd5b0f633f205d23b87
Rafael Campos Nunes <rafaelnunes737@hotmail.com>
2015-10-11 11:29:18 -0300

    Arquivo que expõe um websocket para o cliente usar.

---
+++ src/public/js/sce/sockets.js
@@ -0,0 +1,27 @@
+/**
+ * Script que abre uma conexão com o websocket specificado e define o objeto
+ * sockets para utilização posterior por outro script.
+ */
+
+
+(function () {
+    'use strict'
+
+    var ws,
+        sockets,
+        ws_port = 9005;
+
+
+    ws = new WebSocket('ws://' + window.location.hostname + ":" + ws_port)
+
+    if(ws != null) {
+      sockets = true;
+    }
+    else {
+      console.error('Conexão com o websocket não pôde ser estabelecida, o servidor pode estar fora do ar');
+    }
+
+    window.ws = ws;
+    window.sockets = sockets;
+    window.ws_port = ws_port
+})();
