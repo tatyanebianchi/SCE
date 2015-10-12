74e320e684af108bd5ebd03d006d175e32d2394f
Rafael Campos Nunes <rafaelnunes737@hotmail.com>
2015-10-12 00:25:37 -0300

    script referente a página de empresas.

---
+++ src/public/js/sce/empresas.js
@@ -0,0 +1,67 @@
+/**
+ *
+ */
+
+ // Verificando se o objeto sockets existe na página.
+ if(typeof sockets == "undefined") {
+     throw new Error("This script requires sockets.js, verify if it was included.");
+ }
+else {
+    ws.onopen = function(e) {
+        console.log("Conexão com o web socket bem sucedida na porta %s", ws_port);
+        carregar_lista();
+    }
+
+    ws.onerror = function(e) {
+        console.log("Erro de conexão com o websocket, provavelmente o servidor foi desligado.");
+    }
+
+    ws.onclose = function(e) {
+      console.log("Conexão com o websocket fechada.");
+    }
+
+    ws.onmessage = function(data) {
+        // Transformando os dados em JSON novamente.
+        data = JSON.parse(data.data);
+
+        switch (data.code) {
+            case '1007':
+                switch(data.desc) {
+                    case 'empresas':
+                        $('#tabela-empresas').fadeIn("fast");
+                        for(var i = 0; i < data.value.length; i++) {
+                            $('#tabela-empresas').append(
+                              $(
+                                "<tr><td>"+ data.value[i].nome + "</td>" +
+                                "<td>" + data.value[i].telefone + "</td>" +
+                                "<td>" + data.value[i].email + "</td>" +
+                                "<td class='text-center'><div class='btn-group btn-group-lg' role='group'><button class='btn btn-primary' title='Editar informações da empresa'><i class='libre libre-edit'></i></button>" +
+                                "<button class='btn btn-default' title='Ver informações da empresa'><i class='libre libre-content'></i></button>" +
+                                "<button class='btn btn-danger' title='Excluir empresa'><i class='libre libre-trash'></i></button></div></td></tr>")
+                                .fadeIn("slow"));
+                        }
+                        break;
+                  }
+                break;
+            case '1004':
+                $('p#error_box').html(
+                  '<button type="button" class="close" data-dismiss="alert" title="Clique para fechar">' +
+                  '<span aria-hidden="true"> &times;</span></button> Erro no sistema (1004): ' + data.desc
+                );
+                $('p#error_box').removeClass('hide').fadeIn("slow");
+                break;
+
+        }
+    }
+
+    /**
+     *
+     */
+    function carregar_lista() {
+      // TODO: Capturar e escrever todas as empresas que estão no banco de dados.
+      ws.send(JSON.stringify({
+          code: '1006',
+          desc: 'get_companies'
+      }));
+    }
+}
