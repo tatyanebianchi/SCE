2b2a061c6ad84b7d6b6c97af98dafdb66a1f741e
Rafael Campos Nunes <rafaelnunes737@hotmail.com>
2015-11-10 21:53:58 -0300

    Agora o servidor reinicia em caso de exceção. Exceto também por um problema: EADDRINUSE.

--- src/server/server.js
+++ src/server/server.js
@@ -5,23 +5,23 @@
   *************************************************************************/
 
 // express e middleware
-var express     = require('express');
-var bodyParser  = require('body-parser');
+var express     = require('express'),
+    bodyParser  = require('body-parser');
 var app         = express();
 
 // SCE
-var webSocket   = require('./web_socket.js'),
+var ws          = require('./web_socket.js'),
     login       = require('./login.js'),
     utils       = require('./server_utils.js'),
     cadastro    = require('./cadastro.js');
 
 // node.js
 var path        = require('path'),
+    cluster     = require('cluster');
     node_utils  = require('util');
-    // cluster     = require('cluster');
 
 // Início da escrita no log.
-utils.write_log('\n\n\n\n\n=================== SERVER INIT ====================');
+utils.write_log('\n\n\n\n\n=================== SERVER INIT: ' + Date() + '====================');
 
 if(process.argv[2] == ("-d" || "-debug")) {
     utils.set_debug(true);
@@ -36,80 +36,109 @@ else if(process.argv[2] == '-h') {
     process.exit();
 }
 
-process.on('uncaughtException', function(err) {
-    // construindo a pilha de rastreamento (I know, it sounds weird in portuguese)
-    var stack = err.stack;
+// if(cluster.isMaster) {
+//     // Inicia um processo adjacente ao processo mestre.
+//     cluster.fork();
+//
+//     // Reiniciando o processo se houve exceção.
+//     cluster.on('exit', function(worker, code, signal) {
+//         setTimeout(function() {
+//           if(utils.is_debug()) {
+//               node_utils.log('worker '+ worker.process.pid + ' morreu ('+ (signal || code) + '). Reiniciando...');
+//           }
+//
+//           utils.write_log('Algo sério aconteceu e o cluster está reiniciando o worker.', '904');
+//
+//
+//           cluster.fork();
+//         }, 500);
+//     });
+//
+//     cluster.on('online', function(worker) {
+//         node_utils.log('O worker ' + worker.id + ' está executando.');
+//     });
+// }
+// else {
+    process.on('uncaughtException', function(err) {
+        // construindo a pilha de rastreamento (I know, it sounds weird in portuguese)
+        var stack = err.stack;
 
-    utils.write_log('Exceção: ' + stack, '904');
-    node_utils.log("Exceção: " + stack);
-});
+        ws.send_json({
+          code: '1004',
+          desc: '[INTERNAL_SERVER_ERROR]',
+          value: 'O servidor sofreu um problema grave, por favor, contate o administrador.'
+        })
 
+        utils.write_log('Exceção: ' + stack, '904');
+        node_utils.log("Exceção: " + stack);
+        node_utils.inspect(stack);
+        process.exit(7);
+    });
 
-/**
- * Pasta padrão para os arquivos do cliente
- */
-app.use(express.static('../public'));
-/**
- * Middleware bodyParser
- */
-app.use(bodyParser());
+    /**
+     * Pasta padrão para os arquivos do cliente
+     */
+    app.use(express.static('../public'));
+    /**
+     * Middleware bodyParser
+     */
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
-// Inicializando o web socket.
-webSocket.init();
+    // Inicializando o web socket.
+    ws.init();
 
-app.get("/", function (req, res) {
-    console.log("get on /");
-    // webSocket.init();
-});
+    app.get("/", function (req, res) {
+        console.log("get on /");
+        // webSocket.init();
+    });
 
-app.get("/logout", function(req, res) {
-    // TODO: logout. Destruir sessão e cookies.
-    login.logout(req, res);
-});
+    app.get("/logout", function(req, res) {
+        // TODO: logout. Destruir sessão e cookies.
+        login.logout(req, res);
+    });
 
-app.post("/login", function(req, res) {
-    // TODO: Login. Criar sessão e armazenar cookies
-    login.do_login(req.body, res);
-});
+    app.post("/login", function(req, res) {
+        // TODO: Login. Criar sessão e armazenar cookies
+        login.do_login(req.body, res);
+    });
 
-app.post("/cadastra_empresa", function(req, res) {
-    cadastro.cadastra_empresa(req.body, res);
-});
+    app.post("/cadastra_empresa", function(req, res) {
+        cadastro.cadastra_empresa(req.body, res);
+    });
 
-app.post("/cadastra_estagiario", function(req, res) {
-    cadastro.cadastra_estagiario(req.body, res);
-});
+    app.post("/cadastra_estagiario", function(req, res) {
+        cadastro.cadastra_estagiario(req.body, res);
+    });
 
-app.post("/cadastra_orientador", function(req, res) {
-    cadastro.cadastra_orientador(req.body, res);
-});
+    app.post("/cadastra_orientador", function(req, res) {
+        cadastro.cadastra_orientador(req.body, res);
+    });
 
-app.post("/cadastra_turma", function(req, res) {
-    cadastro.cadastra_turma(req.body, res);
-});
+    app.post("/cadastra_turma", function(req, res) {
+        cadastro.cadastra_turma(req.body, res);
+    });
 
+    /**
+     * Seção do software a ser implementada posteriormente
+     */
+     app.post("/cadastra_usuario", function(req, res) {
+        cadastro.cadastra_usuario(req.body, res);
+     });
 
-/**
- * Seção do software a ser implementada posteriormente
- */
- app.post("/cadastra_usuario", function(req, res) {
-    cadastro.cadastra_usuario(req.body, res);
- });
+    /**
+     * Capturando o erro 404.
+     */
+    app.use(function(req, res, next) {
+      res.status(404);
+      res.sendFile(utils.get_file("404.html"));
+    });
 
-/**
- * Capturando o erro 404.
- */
-app.use(function(req, res, next) {
-  res.status(404);
-  res.sendFile(utils.get_file("404.html"));
-});
+    var server = app.listen(9000, function () {
+      var host = server.address().address;
+      var port = server.address().port;
 
-var server = app.listen(9000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  var env = JSON.stringify( process.env );
-
-  utils.write_log('Servidor executando em: ' + process.cwd(), '900')
-  utils.write_log('Servidor escutando em: http://' + host + ':' + port, '900');
-});
+      utils.write_log('Servidor executando em: ' + process.cwd(), '900')
+      utils.write_log('Servidor escutando em: http://' + host + ':' + port, '900');
+    });
+// }
