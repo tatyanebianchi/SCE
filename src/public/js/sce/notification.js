962506b7553094c2b56b883f8facac262a1421ce
Rafael Campos Nunes <rafaelnunes@engineer.com>
2015-11-26 20:14:07 -0300

    A função esconder notificação agora aceita um parâmtro de delay.
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/public/js/sce/notification.js
+++ src/public/js/sce/notification.js
@@ -1,51 +1,91 @@
 /**
- * notification.js
- * Author: Rafael Campos Nunes.
- * License: GPLv3
+ * Este arquivo pertence ao SCE - Sistema de Controle de Estágio -, cuja função
+ * é realizar o controle de estágio para discentes do IFPA.
+ * Copyright (C) 2015  Rafael Campos Nunes
  *
- * API de notificão do SCE.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+'use strict'
+
 if(typeof jQuery == "undefined") {
-    throw new Error("notification.js needs jQuery to work");
+  throw new Error("notification.js needs jQuery to work");
 }
 else {
-    var animation_time = 500;
+  var animationTime = 500;
 
-    var classes = 'sce-alert sce-alert-danger sce-alert-warning sce-alert-info' +
-                  'sce-alert-success'
+  var classes = 'sce-alert sce-alert-danger sce-alert-warning sce-alert-info' +
+                'sce-alert-success'
 
-    function notificacao_erro(mensagem) {
-        $("#notificacao").addClass("sce-alert sce-alert-danger")
-        .html(mensagem)
-        .slideDown(animation_time)
-        .removeClass("hidden");
-    }
+  $(document).ready(function() {
+  	$('#notificacao').addClass('text-center');
 
-    function notificacao_alerta(mensagem) {
-        $("#notificacao").addClass("sce-alert sce-alert-warning")
-        .html(mensagem)
-        .slideDown(animation_time)
-        .removeClass("hidden");
-    }
+  	/**
+  	 *
+  	 */
+	  window.notificacao_erro = function(mensagem) {
+	    $("#notificacao").addClass("sce-alert sce-alert-danger")
+	    .html(mensagem)
+	    .slideDown(animationTime)
+	    .removeClass("sce-hide");
+	  }
 
-    function notificacao_informacao(mensagem) {
-        $("#notificacao").addClass("sce-alert sce-alert-info")
-        .html(mensagem)
-        .slideDown(animation_time)
-        .removeClass("hidden");
-    }
+	  /**
+  	 *
+  	 */
+	  window.notificacao_alerta = function(mensagem) {
+	    $("#notificacao").addClass("sce-alert sce-alert-warning")
+	    .html(mensagem)
+	    .slideDown(animationTime)
+	    .removeClass("sce-hide");
+	  }
 
-    function notificacao_sucesso(mensagem) {
-        $("#notificacao").addClass("sce-alert sce-alert-success")
-        .html(mensagem)
-        .slideDown(animation_time)
-        .removeClass("hidden");
-    }
+	  /**
+  	 *
+  	 */
+	  window.notificacao_informacao = function(mensagem) {
+	    $("#notificacao").addClass("sce-alert sce-alert-info")
+	    .html(mensagem)
+	    .slideDown(animationTime)
+	    .removeClass("sce-hide");
+	  }
 
-    function esconder_notificacao() {
-        $("#notificacao")
-        .delay(750)
-        .slideUp(animation_time);
-    }
+	  /**
+  	 * 
+  	 */
+	  window.notificacao_sucesso = function(mensagem) {
+	    $("#notificacao").addClass("sce-alert sce-alert-success")
+	    .html(mensagem)
+	    .slideDown(animationTime)
+	    .removeClass("sce-hide");
+	  }
+
+	  /**
+	   * @param delay Se definido, vai ser utilizado como tempo de delay
+	   * para esconder a notificação. Caso contrário, um valor padrão será
+	   * utilizado.
+	   */
+	  window.esconder_notificacao = function(delay) {
+	  	var delayTime = 350;
+
+	  	if (delay !== 'undefined') {
+	  		delayTime = delay;
+	  	}
+
+	    $("#notificacao")
+	    .delay(delayTime)
+	    .slideUp(animationTime);
+	  }
+  });
 }
